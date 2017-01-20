570640b76d5ae88b6b07f8f4a37d9a15f8dd2df9
Tristram Oaten <tristram@oaten.name>
2017-01-20 17:38:01 +0000

    add stub admin

--- gm/admin.py
+++ gm/admin.py
@@ -10,6 +10,32 @@ from .models import (
     News
 )
 
-[admin.site.register(model)
- for model in locals().values()
- if hasattr(model, "pk")]
+
+class CharacterAdmin(admin.ModelAdmin):
+    pass
+
+class SkillAdmin(admin.ModelAdmin):
+    pass
+
+class ContactAdmin(admin.ModelAdmin):
+    pass
+
+class StageAdmin(admin.ModelAdmin):
+    pass
+
+class TeamAdmin(admin.ModelAdmin):
+    pass
+
+class MissionGroupAdmin(admin.ModelAdmin):
+    pass
+
+class NewsAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Contact, ContactAdmin)
+admin.site.register(Stage, StageAdmin)
+admin.site.register(Team, TeamAdmin)
+admin.site.register(MissionGroup, MissionGroupAdmin)
+admin.site.register(News, NewsAdmin)
